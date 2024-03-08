3d301b3bc5f81f0884a4b41bc4c8ff6733ee4f44
HermanYudin <hermanhockey1994@yandex.ru>
2024-03-08 18:39:53 +0300

    init

---
+++ src/components/AppButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="convert()">CONVERT</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    convert: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  min-width: 100px;
+  min-height: 30px;
+  margin-top: 20px;
+  color: deeppink;
+  border: none;
+  font-weight: bold;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+
+button:hover {
+  background-color: grey;
+}
+</style>
---
+++ src/components/AppInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input
+      v-on:input="changeAmount($event.target.value)"
+      min="0"
+      type="number"
+      placeholder="Enter your value"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppInput",
+  props: {
+    changeAmount: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+div input {
+  min-height: 30px;
+  min-width: 300px;
+  padding-right: 10px;
+  padding-left: 10px;
+  outline: none;
+  border-radius: 20px;
+  border: none;
+  color: deeppink;
+}
+</style>
